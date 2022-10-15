57532df0e3f231c4f8643c4265558d72fa50bb14
Asmaa Adel Omar <114780478+asmaaadel39@users.noreply.github.com>
2022-10-15 10:00:42 +0200

    Add files via upload
    Problem#3 - Count Inversions (Merge Sort)
    Given an array A of integers, you're to calculate the number of inversions in this array.
    Number of inversions is the number of pairs (i,j) of array indices with i<j and A[i] > A[j] -

--- main.cpp
+++ main.cpp
@@ -1,70 +1,86 @@
+
 #include <iostream>
+
 using namespace std;
-#include <bits/stdc++.h>
-// given function
-long long m_w, m_z;
-long long get_random() {
-    m_z = 36969 * (m_z & 65535) + (m_z >> 16);
-    m_w = 18000 * (m_w & 65535) + (m_w >> 16);
-    long long res = (m_z << 16) + m_w;
-    return res % 1000000000;
-}
 
-void swap(int *z, int *y) {
-    int  tmp = *z;
-    *z = *y;
-    *y = tmp;
-}
+int m_alg(int left, int right, int middle,int array[] ) {
 
+    int x = 0;
+    int numLeft = middle - left + 1;
+    int numRight = right - middle;
+    int *l = new int[numLeft];
+    int *r = new int[numRight];
+     int idx=0,idx2=0;
+     while( idx < numLeft) {
+        l[idx] = array[left + idx];
+        idx++;
+    }
+    while ( idx2 < numRight) {
+        r[idx2] = array[middle + 1 + idx2];
+        idx2++;
+    }
+    int i = 0, j = 0, y = left;
 
-double divide(int a,int b,int array[])
-{
-    int var = array[b];
-    int  index = a;
-    int j = a;
-    while ( j <= b - 1)
-    {
-
-        if (array[j] <= var) {
-            swap(&array[index], &array[j]);
-            index ++;
+    while (i < numLeft && j < numRight) {
+        if (r[j] < l[i]) {
+            x += numLeft - i;
+            array[y] = r[j];
+            j++;
+        } else {
+            array[y] = l[i];
+            i++;
         }
+        y++;
+    }
+    while ( i < numLeft) {
+        array[y] = l[i];
+        i++;
+    }
+    while ( j < numRight) {
+        array[y] = r[j];
         j++;
     }
-    swap(&array[index], &array[b]);
-    return index;
+    return x;
+
 }
-
-
-
-
-int kthminimum(int array[], int a, int b, int j) {
-
-   int index = divide(a, b, array);
-
-    if ( index- a == j - 1)
-        return array[index];
-    while(index - a > j - 1)
+void printArray(int Array[], int size)
+{
+    int i = 0;
+    while (i < size)
     {
-      index--;
-        return kthminimum(array, a, index, j);
+        cout << Array[i] << " ";
+        i++;
     }
 
-    return kthminimum(array, index + 1, b, j - index + a - 1);
 }
+
+int Merge_Sort(int array[], int const begin, int end) {
+    int element = 0;
+    if (begin >= end) return element;
+    int m = (begin + end) / 2;
+    element += Merge_Sort(array, begin, m);
+    element += Merge_Sort(array, m + 1, end);
+    element += m_alg( begin, end, m,array);
+    return element;
+}
+
 
 int main() {
-    int N,K,array[N],i=0;
-    cin >> N >> K >> m_w >> m_z;
-    while(i<N)
-    {
-        array[i] = get_random();
+    int size;
+    cout<<"Enter size"<<endl;
+    cin >> size;
+    cout<<"Enter Elements"<<endl;
+    int *arr = new int[size];
+    int i = 0;
+    while(i < size) {
+        cin >> arr[i];
         i++;
-
     }
-    cout << kthminimum(array, 0, N - 1, K);
+    cout<<"the number of inversions =";
+    cout << Merge_Sort(arr, 0, size - 1)<<endl;
+ //   printArray(arr,size);
     return 0;
+    // 4
+    // 1 2 3 4
+    // output 0
 }
-
-
-
